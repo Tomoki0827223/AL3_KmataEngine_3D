25ebe892b98a1a726c18bf4d1d8d3b74dea027f6
Tomoki0827223 <146914498+Tomoki0827223@users.noreply.github.com>
2024-05-30 11:09:26 +0900

    rrrr

--- DirectXGame/Player.h
+++ DirectXGame/Player.h
@@ -1,1 +1,25 @@
 #pragma once
+
+#include <Model.h>
+#include <WorldTransform.h>
+
+class Player {
+
+public:
+
+	void Initialize(Model* model, uint32_t textureHandle, ViewProjection* viewProjection);
+
+	void Update();
+
+	void Draw();
+
+private:
+
+	WorldTransform worldTransform_;
+
+	ViewProjection* viewProjection_ = nullptr;
+
+	Model* model_ = nullptr;
+
+	uint32_t textureHandle_ = 0u;
+};
