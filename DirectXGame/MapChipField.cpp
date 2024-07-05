010a1955ad057191c358b2d307d9b786686bd2b3
Tomoki0827223 <146914498+Tomoki0827223@users.noreply.github.com>
2024-07-05 14:39:17 +0900

    これじゃねえか

--- DirectXGame/MapChipField.cpp
+++ DirectXGame/MapChipField.cpp
@@ -6,13 +6,10 @@
 #include <string>
 
 namespace {
-
 std::map<std::string, MapChipType> mapChipTable = {
-
     {"0", MapChipType::kBlank},
     {"1", MapChipType::kBlock},
 };
-
 }
 
 uint32_t MapChipField::GetNumBlockVirtical() { return kNumBlockVirtical; }
--- DirectXGame/MapChipField.h
+++ DirectXGame/MapChipField.h
@@ -20,8 +20,8 @@ struct MapChipData {
 class MapChipField {
 
 public:
-	static inline const float kBlockWidth = 1.0f;
-	static inline const float kBlockHeight = 1.0f;
+	static inline const float kBlockWidth = 2.0f;
+	static inline const float kBlockHeight = 2.0f;
 
 	static inline const uint32_t kNumBlockVirtical = 20;
 	static inline const uint32_t kNumBlockHorizontal = 100;
